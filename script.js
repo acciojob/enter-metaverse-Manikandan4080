ca62ea5606cb741573123bfd528732a1fafc724c
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-09-29 15:41:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 //your JS code here. If required.
-let para = document.getElementById("status")
-let enterBtn = document.getElementById("enterBtn")
+let para = document.getElementById("status");
+let enterBtn = document.getElementById("enterBtn");
 enterBtn.addEventListener("click", change);
 
 function change(){
 	let heading = document.createElement("h1");
+	heading.id = "status"
 	heading.innerText = "Entered Metaverse";
 	document.body.insertBefore(heading, para)
-	para.remove()
+	para.remove();
 }
