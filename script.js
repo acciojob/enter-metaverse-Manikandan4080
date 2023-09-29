a6ecf83b55ac6c9db614bc9e1d29e081d5e2b764
Manikandan4080 <113871926+Manikandan4080@users.noreply.github.com>
2023-09-29 15:21:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+let para = document.createElement("p");
+para.innerText = "Entered Metaverse";
+para.id = "status";
+
+let enterBtn = document.createElement("button");
+enterBtn.innerText = "Enter";
+enterBtn.id = "enterBtn";
+enterBtn.addEventListener("click", change);
+
+document.body.append(para, enterBtn);
+
+function change(){
+	let heading = document.createElement("h1");
+	heading.innerText = "Entered Metaverse";
+	document.body.insertBefore(heading, para)
+	para.remove()
+}
